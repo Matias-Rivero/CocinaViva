d6ec0283d97a75b51cf584a337d65b7fa52ae5fd
Matias-Rivero <rivero.matias.sebastian@gmail.com>
2018-04-21 22:30:40 -0300

    Version Base YoCocinoTodo
    Se sube la version base del proyecto, solo cuenta con una vista basica de login para corroborar que la aplicacion levanta correctamente. la url es http://localhost:8080/proyecto-yct/validar-login

---
+++ proyecto-yct/src/main/java/ar/edu/unlam/yococinotodo/servicios/ServicioLogin.java
@@ -0,0 +1,10 @@
+package ar.edu.unlam.yococinotodo.servicios;
+
+import ar.edu.unlam.yococinotodo.modelo.Usuario;
+
+// Interface que define los metodos del Servicio de Usuarios.
+public interface ServicioLogin {
+
+	Usuario consultarUsuario(Usuario usuario);
+	
+}
